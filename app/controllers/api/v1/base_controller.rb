e78129d5f31c5e00e30df00e15fe1302178ba6bf
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2012-07-23 11:18:18 +0200

    rename after filter header in base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::BaseController < ApplicationController
+  after_filter :set_access_control_origin_headers
 
 private
+
+  def set_access_control_origin_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response.headers['Access-Control-Max-Age'] = "1728000"
+  end
 
   def current_user
     if doorkeeper_token
